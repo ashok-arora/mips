86f3bd2cbc67809d7297278d571940f5a9d4cc78
Ashok Arora <arora8715@gmail.com>
2021-02-28 19:04:44 +0400

    Output file

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
  * 1. read file [done]
  * 2. execute line by line
     ** list of accepted tokens
- * 3. print output to file
+ * 3. print output to file [done]
  * 4. point out error
  */
 
@@ -12,12 +12,21 @@
 #include "tokens.hpp"
 #include "registers.hpp"
 #include <limits>
+#include <algorithm>
 
 int main(int argc, char **argv){
+    std::filebuf fileBuffer;
+    std::ostream output(&fileBuffer);
     if (argc < 2){
-        std::cout << "Expected file as first argument. None found." << std::endl;
+        std::cout << "[Error] Expected input file as first argument. None found." << std::endl;
         return -1;
     }
+    else if (argc < 3){
+        std::cout << "[Warning] Expected output file as second argument! None found. Defaulting to `./build/output.txt`" << std::endl;
+        fileBuffer.open("./build/output.txt", std::ios::out);
+    }
+
+    fileBuffer.open(argv[2], std::ios::out);
     std::string text;
     std::ifstream fileName(argv[1]);
     while(fileName >> text){
@@ -31,8 +40,16 @@ int main(int argc, char **argv){
             std::cout << "Unsupported token: " << text << std::endl;
         }
     }
-    if(registers.find("ra")!= registers.end()){
-        std::cout << "Currently, register $" <<registers.find("ra")->first << " has value " << registers.find("ra")->second << std::endl;
+    std::vector<std::pair<std::string, int>>::iterator findIt = std::find_if(registers.begin(), registers.end(), CompareFirst("ra"));
+    if(findIt != registers.end()) {
+        std::cout << "Currently, register $" << findIt->first << " has value " << findIt->second << std::endl;
+    } else {
+        std::cout << "Register $ra not found. " <<std::endl;
+    }
+
+    std::vector<std::pair<std::string, int>>::iterator iter;
+    for (iter = registers.begin() ; iter != registers.end() ; iter++){
+        output << iter->first << " = " << iter->second << std::endl;
     }
 
     return 0;
--- src/registers.hpp
+++ src/registers.hpp
@@ -6,39 +6,49 @@
 #define MIPS_REGISTERS_HPP
 
 #include <unordered_map>
+#include <vector>
 
-std::unordered_map <std::string, int> registers({
-                                                        {"$zero", 0},
-                                                        {"at", 0},
-                                                        {"v0", 0},
-                                                        {"v1", 0},
-                                                        {"a0", 0},
-                                                        {"a1", 0},
-                                                        {"a2", 0},
-                                                        {"a3", 0},
-                                                        {"t0", 0},
-                                                        {"t1", 0},
-                                                        {"t2", 0},
-                                                        {"t3", 0},
-                                                        {"t4", 0},
-                                                        {"t5", 0},
-                                                        {"t6", 0},
-                                                        {"s0", 0},
-                                                        {"s1", 0},
-                                                        {"s2", 0},
-                                                        {"s3", 0},
-                                                        {"s4", 0},
-                                                        {"s5", 0},
-                                                        {"s6", 0},
-                                                        {"s7", 0},
-                                                        {"t8", 0},
-                                                        {"t9", 0},
-                                                        {"k0", 0},
-                                                        {"k1", 0},
-                                                        {"gp", 0},
-                                                        {"sp", 0},
-                                                        {"fp", 0},
-                                                        {"ra", 0},
+std::vector <std::pair<std::string, int>> registers {
+            {"zero", 0},
+            {"at", 0},
+            {"v0", 0},
+            {"v1", 0},
+            {"a0", 0},
+            {"a1", 0},
+            {"a2", 0},
+            {"a3", 0},
+            {"t0", 0},
+            {"t1", 0},
+            {"t2", 0},
+            {"t3", 0},
+            {"t4", 0},
+            {"t5", 0},
+            {"t6", 0},
+            {"s0", 0},
+            {"s1", 0},
+            {"s2", 0},
+            {"s3", 0},
+            {"s4", 0},
+            {"s5", 0},
+            {"s6", 0},
+            {"s7", 0},
+            {"t8", 0},
+            {"t9", 0},
+            {"k0", 0},
+            {"k1", 0},
+            {"gp", 0},
+            {"sp", 0},
+            {"fp", 0},
+            {"ra", 0},
+};
 
-                                                });
+struct CompareFirst {
+    CompareFirst(std::string val) : val_(val) {}
+    bool operator()(const std::pair<std::string,int>& elem) const {
+        return val_ == elem.first;
+    }
+private:
+    std::string val_;
+};
+
 #endif //MIPS_REGISTERS_HPP
